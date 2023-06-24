0667bf30502a4741306984a7cf0453d5346f8768
Eddwin Cheteni <eddychetz@gmail.com>
2023-06-24 21:43:02 +0200

    update requirements

---
+++ 00_scripts/requirements.R
@@ -0,0 +1,11 @@
+pkgs <- c(
+    "here",
+    "gt",
+    "shinyWidgets",
+    "remotes"
+)
+
+install.packages(pkgs)
+
+remotes::install_github("mdancho84/gridlayout")
+remotes::install_github("mdancho84/shinyuieditor")
